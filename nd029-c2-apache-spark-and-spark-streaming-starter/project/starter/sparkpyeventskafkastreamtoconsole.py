f58dbe5a126825753ed5b4bddb567df9621b8915
fxrcode <fxrcode@gmail.com>
2021-06-15 16:56:44 -0700

    complete 2nd pyspark: to read stedi-events and select only customer&score and writeStream to console

--- nd029-c2-apache-spark-and-spark-streaming-starter/project/starter/sparkpyeventskafkastreamtoconsole.py
+++ nd029-c2-apache-spark-and-spark-streaming-starter/project/starter/sparkpyeventskafkastreamtoconsole.py
@@ -1,11 +1,20 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, unbase64, base64, split
-from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
+from pyspark.sql.types import FloatType, StructField, StructType, StringType, BooleanType, ArrayType, DateType
 
 # TO-DO: using the spark application object, read a streaming dataframe from the Kafka topic stedi-events as the source
 # Be sure to specify the option that reads all the events from the topic including those that were published before you started the spark stream
-                                   
-# TO-DO: cast the value column in the streaming dataframe as a STRING 
+spark = SparkSession.builder.appName("stedi-events").getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
+
+# TO-DO: cast the value column in the streaming dataframe as a STRING
+stediEventsRawStreamingDF = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "stedi-events") \
+    .option("startingOffsets", "earliest") \
+    .load()
 
 # TO-DO: parse the JSON from the single column "value" with a json object in it, like this:
 # +------------+
@@ -20,11 +29,31 @@ from pyspark.sql.types import StructField, StructType, StringType, BooleanType, 
 # +------------+-----+-----------+
 # |"sam@tes"...| -1.4| 2020-09...|
 # +------------+-----+-----------+
-#
+stediEventsStreamingDF = stediEventsRawStreamingDF.selectExpr("cast(value as string) value")
+stediEventsSchema = StructType(
+    [
+        StructField("customer", StringType()),
+        StructField("score", FloatType()),
+        StructField("riskDate", DateType())
+    ]
+)
+
 # storing them in a temporary view called CustomerRisk
+stediEventsStreamingDF.withColumn("value", from_json("value", stediEventsSchema)) \
+        .select(col("value.*")) \
+        .createOrReplaceTempView("CustomerRisk")
+
 # TO-DO: execute a sql statement against a temporary view, selecting the customer and the score from the temporary view, creating a dataframe called customerRiskStreamingDF
+customerRiskStreamingDF = spark.sql("select customer, score from CustomerRisk")
+
 # TO-DO: sink the customerRiskStreamingDF dataframe to the console in append mode
-# 
+consoleOutput = customerRiskStreamingDF \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option("truncate", False) \
+    .start() \
+    .awaitTermination()
 # It should output like this:
 #
 # +--------------------+-----
@@ -34,4 +63,4 @@ from pyspark.sql.types import StructField, StructType, StringType, BooleanType, 
 # +--------------------+-----
 # Run the python script by running the command from the terminal:
 # /home/workspace/submit-event-kafka-streaming.sh
-# Verify the data looks correct 
+# Verify the data looks correct
